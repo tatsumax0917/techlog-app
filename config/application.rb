9e906199c7142c713e417fd28cf99e30019708b9
tatsumax <tatsumax.development@gmail.com>
2024-01-08 14:01:38 +0000

    rails generateeee

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module TechlogApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

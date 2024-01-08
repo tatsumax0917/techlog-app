077f435a110bc9c57bb15eea5c73d6a63e6fd76b
tatsumax <tatsumax.development@gmail.com>
2024-01-08 13:39:18 +0000

    project start

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "8b2aad9a29b74db9b36577f26028106a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
 end

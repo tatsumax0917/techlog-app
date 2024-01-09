d652dc12ae5577a591d1672af4cc8a2ab49416d1
tatsumax <tatsumax.development@gmail.com>
2024-01-09 02:13:10 +0000

    RSpecの初期設定を完了

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :development do
   gem 'rubocop-performance', require: false
   gem 'rubocop-rails', require: false
   gem 'rubocop-rspec'
+  gem 'spring-commands-rspec'
   # Add speed badges [https://github.com/MiniProfiler/rack-mini-profiler]
   # gem "rack-mini-profiler"
 
--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,8 @@ module TechlogApp
       g.helper false
       g.test_framework :rspec,
         fixture: false,
-        view_specs: false
-        helper_specs: false
+        view_specs: false,
+        helper_specs: false,
         routing_specs: false
     end
     # Configuration for the application, engines, and railties goes here.

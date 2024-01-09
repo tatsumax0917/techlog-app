404e3f95749011f878b75836d55b89fff71a865f
tatsumax <tatsumax.development@gmail.com>
2024-01-09 07:59:38 +0000

    トップページの統合テストを追加

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'Top page Verification' do
+    it 'Home#top should be displayed' do
+      visit '/'
+      expect(page).to have_content('Home#top')
+    end
+  end
+
+  # pending "add some scenarios (or delete) #{__FILE__}"
+end

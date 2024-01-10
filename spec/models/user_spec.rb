fae906e31dbc64690c7240fb6aa341738d1089cf
tatsumax <tatsumax.development@gmail.com>
2024-01-10 11:13:34 +0000

    Postモデルを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,8 @@ describe User do
 
   describe '.first' do
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, email: email)
+      @post = create(:post, title: 'タイトル', content: '本文', user_id: @user.id)
     end
 
     subject { described_class.first }
@@ -16,43 +17,50 @@ describe User do
       expect(subject.nickname).to eq('テスト太郎')
       expect(subject.email).to eq('test@example.com')
     end
+
+    it '紐づくPostの情報を取得できる' do
+      expect(subject.posts.size).to eq(1)
+      expect(subject.posts.first.title).to eq('タイトル')
+      expect(subject.posts.first.content).to eq('本文')
+      expect(subject.posts.first.user_id).to eq(@user.id)
+    end
   end
-  
+
   describe 'validation' do
     let(:password) { '12345678' }
-    
+
     describe 'nickname属性' do
       describe '文字数制限の検証' do
         context 'nicknameが20文字以下の場合' do
-          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' } 
-          
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてと' }
+
           it 'Userオブジェクトは有効である' do
             expect(user.valid?).to be(true)
           end
         end
-        
+
         context 'nicknameが20文字以上の場合' do
-          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' } 
+          let(:nickname) { 'あいうえおかきくけこさしすせそたちつてとな' }
           it 'Userオブジェクトは無効である' do
             user.valid?
-            
+
             expect(user.valid?).to be(false)
             expect(user.errors[:nickname]).to include('は20文字以下に設定して下さい。')
           end
         end
       end
-      
+
       describe 'nickname存在性の検証' do
         context 'nicknameが空欄の場合' do
           let(:nickname) { '' }
-          
+
           it 'User オブジェクトは無効である' do
             expect(user.valid?).to be(false)
            expect(user.errors[:nickname]).to include("が入力されていません。")
           end
         end
       end
-      
+
     end
   end
 end
